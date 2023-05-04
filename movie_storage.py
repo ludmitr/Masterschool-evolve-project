69ed24f39847fec8a68456431a90cb3a0b676e11
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-05-04 10:15:54 +0300

    creating movie_storage.py

---
+++ movie_storage.py
@@ -0,0 +1,68 @@
+import json
+FILE_PATH = "data.json"
+
+
+def load_data():
+    """
+    Returns a dictionary that contains
+    the movies information in the database.
+
+    The function loads the information from the JSON
+    file and returns the data.
+
+    For example, the function may return:
+    {
+      "Titanic": {
+        "rating": 9,
+        "year": 1999
+      },
+      "..." {
+        ...
+      },
+    }
+    """
+    with open("FILE_PATH", "r") as file:
+        return json.loads(file.read())
+
+
+def add_movie(title, year, rating):
+    """
+    Adds a movie to the movies database.
+    Loads the information from the JSON file, add the movie,
+    and saves it. The function doesn't need to validate the input.
+    """
+    movies: dict = load_data()
+    movies[title] = {"rating": rating, "year": year}
+
+    save_data(movies)
+
+
+def delete_movie(title):
+    """
+    Deletes a movie from the movies database.
+    Loads the information from the JSON file, deletes the movie,
+    and saves it. The function doesn't need to validate the input.
+    """
+    movies = load_data()
+    if title in movies:
+        del movies[title]
+
+    save_data(movies)
+
+
+def update_movie(title, rating):
+    """
+    Updates a movie from the movies database.
+    Loads the information from the JSON file, updates the movie,
+    and saves it. The function doesn't need to validate the input.
+    """
+    movies = load_data()
+    movies[title]["rating"] = rating
+
+    save_data(movies)
+
+
+def save_data(movies: dict):
+    """Serialize the movies data in data.json"""
+    with open("data.json", "w") as file:
+        file.write(json.dumps(movies))
