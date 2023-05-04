0511bac2c1a4e6df01309b9d7de7027959e73dd4
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-05-04 20:26:16 +0300

    connected my app with omdbapi. implemented new features

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from matplotlib import pyplot as plt
 from fuzzywuzzy import fuzz
 from colorama import Fore
 import movie_storage
-import omdbapi_api_handler
-import web_generator
 
 MY_OS = sys.platform
 
@@ -23,7 +21,7 @@ def main():
     # continuously displays the main menu, takes user input, and execute
     while True:
         print_menu()
-        user_input = input(Fore.LIGHTBLUE_EX + "Enter choice (0-10): " + Fore.RESET)
+        user_input = input(Fore.LIGHTBLUE_EX + "Enter choice (0-9): " + Fore.RESET)
         execute_user_input(user_input)
 
 
@@ -45,22 +43,11 @@ def execute_user_input(user_input: str) -> None:
         "6": print_random_movie_screen,
         "7": search_movie_by_name_screen,
         "8": print_sorted_movies_by_rating_screen,
-        "9": create_histogram_in_file_screen,
-        "10": generate_website_screen
+        "9": create_histogram_in_file_screen
     }
 
     if user_input in menu_functions_dict:
         menu_functions_dict[user_input]()
-
-
-def generate_website_screen():
-    """Generate website"""
-    print_clear_screen_and_menu_title()
-
-    web_generator.generate_web()
-    print("Website was generated successfully")
-
-    user_input_press_enter_to_continue()
 
 
 def exit_program():
@@ -176,20 +163,18 @@ def add_movie_screen(movie_name: str = None):
     else:
         input_movie_name = user_input_text("Enter a new movie name: ")
 
-    # input_movie_rating = user_input_text("Enter new movie rating: ")
-    # input_movie_year = user_input_text("Enter the release year: ")
-    search_result = omdbapi_api_handler.search_by_title(input_movie_name)
+    input_movie_rating = user_input_text("Enter new movie rating: ")
+    input_movie_year = user_input_text("Enter the release year: ")
+
     print_clear_screen_and_menu_title()
 
     # print message depends on if the year and rating is valid
-    if search_result["Response"] == "True":
-        movie_title = search_result["Title"]
-        movie_year, image_url = int(search_result["Year"]), search_result["Poster"]
-        movie_rating = float((search_result["Ratings"][0]["Value"]).split("/")[0])
-        movie_storage.add_movie(movie_title, movie_year, movie_rating, image_url)
+    if is_movie_rating_valid(input_movie_rating) and is_movie_year_valid(input_movie_year):
+        movie_rating, movie_year = round(float(input_movie_rating), 1), int(input_movie_year)
+        movie_storage.add_movie(input_movie_name, movie_year, movie_rating)
         print(f"Movie {input_movie_name} successfully added/updated")
     else:
-        print(error_text_red_color(f"{search_result['Error']}"))
+        print(error_text_red_color("Invalid data."))
 
     user_input_press_enter_to_continue()
 
@@ -257,14 +242,12 @@ def print_stats_screen() -> None:
     average_rating = round(sum(data["rating"] for data in movies.values()) / len(movies), 1)
     median_rating = round(statistics.median(data["rating"] for data in movies.values()), 1)
     best_movie_name = max(movies, key=lambda movie: movies[movie]["rating"])
-    best_movie_data = {"rating": movies[best_movie_name]["rating"], "year": movies[best_movie_name]["year"]}
     worst_movie_name = min(movies, key=lambda movie: movies[movie]["rating"])
-    worst_movie_data = {"rating": movies[worst_movie_name]["rating"], "year": movies[worst_movie_name]["year"]}
 
     stats_string = f"""Average rating: {average_rating}
 Median rating: {median_rating}
-Best movie: {best_movie_name} {best_movie_data}
-Worst movie: {worst_movie_name}, {worst_movie_data}"""
+Best movie: {best_movie_name}, {movies[best_movie_name]}
+Worst movie: {worst_movie_name}, {movies[worst_movie_name]}"""
 
     print_clear_screen_and_menu_title()
     print(stats_string)
@@ -276,9 +259,7 @@ def print_random_movie_screen():
     movies = movie_storage.load_data()
     random_movie_name = random.choice(list(movies.keys()))
     print_clear_screen_and_menu_title()
-    random_movie_data = {"rating": movies[random_movie_name]["rating"],
-                         "year": movies[random_movie_name]["year"]}
-    print(f"Your movie for tonight: {random_movie_name}  {random_movie_data}")
+    print(f"Your movie for tonight: {random_movie_name}  {movies[random_movie_name]}")
     user_input_press_enter_to_continue()
 
 
@@ -301,9 +282,8 @@ def search_movie_by_fuzzy_matching(movies: dict, input_movie_name: str) -> list:
 def create_str_for_found_movies(found_movies_part_name: dict) -> str:
     """create a string that represent found movies and returns it"""
     output_string = ""
-    for movie, data in found_movies_part_name.items():
-        movie_data = {"rating": data["rating"], "year": data["year"]}
-        output_string += f"{movie}, {movie_data}\n"
+    for movie, rating in found_movies_part_name.items():
+        output_string += f"{movie}, {rating}\n"
 
     return output_string.rstrip()  # removing last \n
 
@@ -387,7 +367,6 @@ def print_menu():
 7. Search movie
 8. Movies sorted by rating
 9. Create Rating Histogram
-10.Generate website
 """
     print(menu_string)
 
--- omdbapi_api_handler.py
+++ omdbapi_api_handler.py
@@ -2,7 +2,10 @@ import requests
 
 API_KEY = "f994fda"
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 05efbd8 (connected my app with omdbapi. implemented new features)
 def search_by_title(title: str) -> dict:
     """Search request for movie title. Returns dict with data of found movie"""
     url = f"https://www.omdbapi.com/?apikey={API_KEY}&t={title}"
