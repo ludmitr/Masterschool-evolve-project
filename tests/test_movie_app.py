8ae7b7a824816fd7e2801547f3aef8422e243f1b
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-05-30 22:31:20 +0300

    deleted files check

--- tests/test_movie_app.py
+++
@@ -1,7 +0,0 @@
-from movie_app import MovieApp
-from storage_json import StorageJson
-
-storage = StorageJson('../data.json')
-movie_app = MovieApp(storage)
-
-assert isinstance(movie_app, MovieApp)
